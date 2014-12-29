1c0b0e0e3b1b059312fa4c599d16d0a18946b31f
Zack Elliott <hubridnoxx@gmail.com>
2014-12-28 18:05:33 -0800

    add schedule interview

--- public/js/global.js
+++ public/js/global.js
@@ -15,7 +15,8 @@ $('.card').click(function(e) {
     $(this).append('<div class=\'on-append\'>' +
     '<a href=\'\'><i class=\'oi\' data-glyph=\'person\'></i></a>' +
     // '<span class=\'open-modal\' data=\'.contact-modal\'><i class=\'oi\' data-glyph=\'phone\'></i></span>' +
-    '<a href=\'\'><i class=\'oi\' data-glyph=\'calendar\'></i></a>' +
+    '<span class=\'open-modal\' data=\'.schedule-modal\'><i class=\'oi\' data-glyph=\'calendar\'></i></span>' +
+    '<span class=\'open-modal\' data=\'.advance-modal\'><i class=\'oi\' data-glyph=\'arrow-thick-right\'></i></span>' +
     '<span class=\'open-modal\' data=\'.delete-modal\'><i class=\'oi\' data-glyph=\'x\'></i></span>' +
     '</div>');
     $(this).find('.upcoming-event').hide();
