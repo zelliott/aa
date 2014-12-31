60175dda050816e98b4d4388db4ce270dbb4bf0e
Zack Elliott <hubridnoxx@gmail.com>
2014-12-30 19:49:48 -0800

    small link changes

--- public/js/global.js
+++ public/js/global.js
@@ -5,21 +5,26 @@ $('.card').click(function(e) {
   if($(e.target).is('.open-modal')) {
     var data = $(e.target).attr('data');
     $(data).show();
+  } else if($(e.target).parent().is('.open-modal')) {
+    var data = $(e.target).parent().attr('data');
+    $(data).show();
   }
 
   if($(this).hasClass('on')) {
     $(this).find('.on-append').remove();
     $(this).find('.upcoming-event').show();
+    $(this).find('.additional-info').show();
     $(this).removeClass('on');
   } else {
     $(this).append('<div class=\'on-append\'>' +
-    '<a href=\'\'><i class=\'oi\' data-glyph=\'person\'></i></a>' +
+    '<a href=\'/candidate\'><i class=\'oi\' data-glyph=\'person\'></i></a>' +
     // '<span class=\'open-modal\' data=\'.contact-modal\'><i class=\'oi\' data-glyph=\'phone\'></i></span>' +
     '<span class=\'open-modal\' data=\'.schedule-modal\'><i class=\'oi\' data-glyph=\'calendar\'></i></span>' +
     '<span class=\'open-modal\' data=\'.advance-modal\'><i class=\'oi\' data-glyph=\'arrow-thick-right\'></i></span>' +
     '<span class=\'open-modal\' data=\'.delete-modal\'><i class=\'oi\' data-glyph=\'x\'></i></span>' +
     '</div>');
     $(this).find('.upcoming-event').hide();
+    $(this).find('.additional-info').hide();
     $(this).addClass('on');
   }
 });
@@ -32,3 +37,14 @@ $('.open-modal').click(function() {
 $('.modal-actions button').click(function() {
   $(this).parent().parent().hide();
 });
+
+$(document).ready(function () {
+  var path = window.location.pathname;
+  if(path == '/today') {
+    $('.tab_2').addClass('tab-on');
+  } else if(path == '/') {
+    $('.tab_1').addClass('tab-on');
+  } else if(path == '/activity') {
+    $('.tab_3').addClass('tab-on');
+  }
+})
